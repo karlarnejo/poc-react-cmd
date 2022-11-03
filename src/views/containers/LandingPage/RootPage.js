f338d9c9a91d774e13eb243d9baff8eab4930270
Karl Anthony Arnejo <karlanthonyarnejo@gmail.com>
2022-11-03 19:29:17 +0800

    initial commit for file path

--- src/views/containers/LandingPage/RootPage.js
+++ src/views/containers/LandingPage/RootPage.js
@@ -9,9 +9,23 @@ const RootPage = () => {
 
     const [command, setCommand] = useState("")
     const [output, setOutput] = useState([])
+    const [fileNames, setFileNames] = useState([])
 
     const handleOnchange = (e) => {
         setCommand(e.target.value)
+    }
+
+    const handleOnchangeFile = (e) => {
+        console.log(e.target.files)
+        //convert from array-like to array
+        let files = [...e.target.files]
+        let tmpArr = []
+
+        files.map((fileName) => {
+            tmpArr.push(fileName.name)
+        })
+        
+        setFileNames(tmpArr)
     }
 
     const submitForm = (e) => {
@@ -26,6 +40,10 @@ const RootPage = () => {
                 setOutput(oldVal => [...oldVal, response])
             })
     }
+
+    useEffect(() => {
+        console.log("eee", fileNames)
+    }, [fileNames]) //eslint-disable-line
 
     useEffect(() => {
         
@@ -76,6 +94,38 @@ const RootPage = () => {
                     </Row>
                 </Tab>
                 <Tab eventKey="fileChooser" title="File Chooser">
+                <Row>
+                        <Col xs={12}>
+                            <Card>
+                                <Form className='' onSubmit={submitForm}>
+                                    <Form.Group className='m-2'>
+                                        <Form.Label>{"Files"}</Form.Label>
+                                        <Form.Control
+                                            type="file"
+                                            // value={command}
+                                            size='sm'
+                                            disabled={false}
+                                            // placeholder={"Enter your command"}
+                                            multiple="multiple"
+                                            onChange={handleOnchangeFile}
+                                        />
+                                    </Form.Group>
+                                </Form>
+                                <hr/>
+                                    <Card>
+                                        {output.map((value, key) => {
+                                            let splitted = value.split("\n")
+                                            return(
+                                                <div key={key}>
+                                                    <div>{">" + splitted[0]}</div>
+                                                    <div>{splitted[1]}</div>
+                                                </div>
+                                            )
+                                        })}
+                                    </Card>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Tab>
             </Tabs>
 
